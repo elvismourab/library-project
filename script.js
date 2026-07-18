4cd5cfb17ac02a8c6877ff163bac97a5aa197f69
elvismourab <216804366+elvismourab@users.noreply.github.com>
2026-07-17 22:51:44 -0300

    feat: add book to array

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const myLibrary = [
-    { id: crypto.randomUUID(), title: "The Hobbit", author: "J.R.R. Tolkien", number: 1, isReaded: true },
-    { id: crypto.randomUUID(), title: "Neuromancer", author: "William Gibson", number: 2, isReaded: false },
-    { id: crypto.randomUUID(), title: "Dune", author: "Frank Herbert", number: 3, isReaded: true },
-    { id: crypto.randomUUID(), title: "Project Hail Mary", author: "Andy Weir", number: 4, isReaded: false },
-    { id: crypto.randomUUID(), title: "Tomorrow, and Tomorrow, and Tomorrow", author: "Gabrielle Zevin", number: 5, isReaded: true }
+    { id: crypto.randomUUID(), title: "The Hobbit", author: "J.R.R. Tolkien", number: 1, isRead: true },
+    { id: crypto.randomUUID(), title: "Neuromancer", author: "William Gibson", number: 2, isRead: false },
+    { id: crypto.randomUUID(), title: "Dune", author: "Frank Herbert", number: 3, isRead: true },
+    { id: crypto.randomUUID(), title: "Project Hail Mary", author: "Andy Weir", number: 4, isRead: false },
+    { id: crypto.randomUUID(), title: "Tomorrow, and Tomorrow, and Tomorrow", author: "Gabrielle Zevin", number: 5, isRead: true }
 ];
 
 function Book(
     title,
     author,
-    number,
-    isReaded,
+    isRead,
 ) {
     if (!new.target) {
         throw new Error("You must use the 'new' operator to call the constructor");
@@ -19,13 +18,22 @@ function Book(
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
-    this.number = number;
-    this.isReaded = isReaded;
+    this.number = myLibrary.length + 1;
+    this.isRead = isRead;
+
+    this.archieve = function() {
+        myLibrary.push(this);
+    }
 }
 
 function addBookToLibrary() {
-    // take params, create a book then store it in the array
-    console.log("book added");
+    const title = document.getElementById("book-title").value;
+    const author = document.getElementById("author").value;
+    const isRead = document.getElementById("is-read").checked;
+
+    const newBook = new Book(title, author, isRead);
+    newBook.archieve();
+    console.log(newBook);
 }
 
 function renderBooks() {
@@ -36,7 +44,7 @@ function renderBooks() {
             <h2>${book.title}</h2>
             <ul>
                 <li>${book.author}</li>
-                <li>${book.isReaded}</li>
+                <li>${book.isRead}</li>
                 <li>${book.number}</li>
             </ul>
         </div>`;
