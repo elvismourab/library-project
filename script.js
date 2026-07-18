de97f1cafbdee3aed941d70b0b7da7dca75da101
elvismourab <216804366+elvismourab@users.noreply.github.com>
2026-07-17 22:15:47 -0300

    feat: add modal-dialog + js events

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const myLibrary = [
-    { id: crypto.randomUUID(), title: "The Hobbit", author: "J.R.R. Tolkien", number: 1, isRead: true },
-    { id: crypto.randomUUID(), title: "Neuromancer", author: "William Gibson", number: 2, isRead: false },
-    { id: crypto.randomUUID(), title: "Dune", author: "Frank Herbert", number: 3, isRead: true },
-    { id: crypto.randomUUID(), title: "Project Hail Mary", author: "Andy Weir", number: 4, isRead: false },
-    { id: crypto.randomUUID(), title: "Tomorrow, and Tomorrow, and Tomorrow", author: "Gabrielle Zevin", number: 5, isRead: true }
+    { id: crypto.randomUUID(), title: "The Hobbit", author: "J.R.R. Tolkien", number: 1, isReaded: true },
+    { id: crypto.randomUUID(), title: "Neuromancer", author: "William Gibson", number: 2, isReaded: false },
+    { id: crypto.randomUUID(), title: "Dune", author: "Frank Herbert", number: 3, isReaded: true },
+    { id: crypto.randomUUID(), title: "Project Hail Mary", author: "Andy Weir", number: 4, isReaded: false },
+    { id: crypto.randomUUID(), title: "Tomorrow, and Tomorrow, and Tomorrow", author: "Gabrielle Zevin", number: 5, isReaded: true }
 ];
 
 function Book(
     title,
     author,
     number,
-    isRead,
+    isReaded,
 ) {
     if (!new.target) {
         throw new Error("You must use the 'new' operator to call the constructor");
@@ -20,11 +20,12 @@ function Book(
     this.title = title;
     this.author = author;
     this.number = number;
-    this.isRead = isRead;
+    this.isReaded = isReaded;
 }
 
 function addBookToLibrary() {
     // take params, create a book then store it in the array
+    console.log("book added");
 }
 
 function renderBooks() {
@@ -35,7 +36,7 @@ function renderBooks() {
             <h2>${book.title}</h2>
             <ul>
                 <li>${book.author}</li>
-                <li>${book.isRead}</li>
+                <li>${book.isReaded}</li>
                 <li>${book.number}</li>
             </ul>
         </div>`;
@@ -45,4 +46,25 @@ function renderBooks() {
     bookshelf.innerHTML = html; // xss !!!
 }
 
+function renderModal() {
+    const showDialog = document.getElementById("show-dialog");
+    const modalDialog = document.getElementById("modal-dialog");
+    const addBook = document.getElementById("add-book");
+    const cancelBtn = document.getElementById("cancel");
+
+    showDialog.addEventListener("click", () => {
+        modalDialog.showModal();
+    })
+
+    cancelBtn.addEventListener("click", () => {
+        modalDialog.close();
+    });
+
+    addBook.addEventListener("submit", (e) => {
+        e.preventDefault();
+        addBookToLibrary();
+    })
+}
+
 renderBooks();
+renderModal();
